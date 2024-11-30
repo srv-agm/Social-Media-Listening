67f3330a7ca918a6f8cbb1f9c82e1a1ff2e1b5fd
deepshikha-satsangi <67851193+deepshikha-satsangi@users.noreply.github.com>
2024-11-30 19:02:24 +0530

    changes

--- src/app/(main)/config/dashboard/page.tsx
+++ src/app/(main)/config/dashboard/page.tsx
@@ -1,12 +1,18 @@
 "use client";
+import MentionsByPlatform from "../../component/graph1";
 
 export default function DashboardPage() {
   return (
     <div className="container mx-auto p-4">
-      <h1 className="mb-6 text-2xl font-bold">Dashboard</h1>
+      <h1 className="mb-6 text-2xl font-bold">
+      <MentionsByPlatform/>
+      <MentionsByPlatform/>
+      <MentionsByPlatform/>
+      </h1>
       <div className="grid gap-4">
         {/* Add your dashboard content here */}
       </div>
     </div>
   );
 }
+// dashboard changes here 
